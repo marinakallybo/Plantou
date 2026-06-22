f4225b05a6e774ebc115f5146726868169c4954d
marinakallybo <marinakally@gmail.com>
2026-06-22 13:56:19 -0300

    Criação do modelo da cultura de sementes

--- recomendador/models.py
+++ recomendador/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Cultura(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    temperatura_minima = models.FloatField()
+    temperatura_maxima = models.FloatField()
+    solo_ideal = models.CharField(max_length=100)
+    umidade_ideal = models.CharField(max_length=50)
+    epoca_plantio = models.CharField(max_length=100)
+    tempo_colheita_dias = models.IntegerField()
+    dificuldade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
